236436e4daad2ec0068ace16d32ae1301ba3bf09
Shakrah Yves <7shakrahs@gmail.com>
2014-02-28 23:27:16 -0800

    py_hardway in progress

---
+++ exercises.py
@@ -0,0 +1,33 @@
+#ex 1.
+print "do I really have to start with 'Hello world?'"
+# I want to skip to string formatting! :(
+# I"m going to skip ahead...
+'''hey what about this thingy?'''
+
+#ex2.
+'''+ does addition
+- does subtraction
+/ divison
+% modulo!! determines the reaminder 
+* multiplication 
+< less than
+> greater than
+<= less than or equal
+>= greater than or equal'''
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "roosters", 100 - 25 * 3 % 4
+
+print "now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+
+#ex3. skip
+
+#ex4. skip
+
+#ex5. 
+
+print "this string is as silly as {0} and {1}".format('David', 'shakrah')
